ff4dfe0b0ff735a608772f98d9277830b4a6f9e0
Tompkins <1011047623@qq.com>
2014-03-16 12:08:25 +0800

    为Poll和Choice类__unicode__方法，利用运算符,盖上Poll.objects.all()的输出。

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=200)     # CharField用来表示字符型字段
     pub_date = models.DateTimeField('date published') # DateField表示日期类型的字段。
+    def __unicode__(self):
+        return self.question
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+    def __unicode__(self):
+        return self.choice
