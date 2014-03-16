bb8fecfaef0538c788332323d5e2b56b12b74e3c
Tompkins <1011047623@qq.com>
2014-03-16 11:29:42 +0800

    定义投票程序中的两个模型：问题和选项。

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,11 @@
+# -*- coding:utf-8 -*-
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)     # CharField用来表示字符型字段
+    pub_date = models.DateTimeField('date published') # DateField表示日期类型的字段。
+
+class Choice(models.Model):
+    poll = models.ForeinKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
